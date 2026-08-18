6026094e26818cc1ecc5aee5806a082349940e59
Jujjuvarapu-Nikhila <jujjuvarapunikhila236@gmail.com>
2026-08-18 11:55:11 +0530

    Changed tac_to_mips

--- 04-08-2026/tac_to_mips.py
+++ 04-08-2026/tac_to_mips.py
@@ -93,7 +93,8 @@ class MIPSGenerator:
         Use self.symbol_table.getSymbol(name).getOffset() to get the
         integer offset, then format it as f"{offset}($fp)".
         """
-        raise NotImplementedError("implement MIPSGenerator.resolve_address()")
+        return f"{self.symbol_table.getSymbol(name).getOffset()}($fp)"
+        ##raise NotImplementedError("implement MIPSGenerator.resolve_address()")
 
     def load(self, operand, reg):
         """
@@ -108,7 +109,13 @@ class MIPSGenerator:
         by reusing the already-live register from triple_index_to_reg,
         never by loading from memory.
         """
-        raise NotImplementedError("implement MIPSGenerator.load()")
+        if is_literal(operand):
+            self.addMIPS(f"li {reg}, {operand}")
+        else:
+            self.addMIPS(f"lw {reg}, {self.resolve_address(operand)}")
+
+        return reg
+        ##raise NotImplementedError("implement MIPSGenerator.load()")
 
     def store(self, reg, name):
         """
@@ -116,7 +123,13 @@ class MIPSGenerator:
         deallocate_register(reg) -- once a value has been written back
         to a variable's slot, the register holding it is free to reuse.
         """
-        raise NotImplementedError("implement MIPSGenerator.store()")
+        """
+        Once a value has been written back to a variable's slot,
+        the register holding it is free to reuse.
+        """
+        self.addMIPS(f"sw {reg}, {self.resolve_address(name)}")
+        self.deallocate_register(reg)
+        ##raise NotImplementedError("implement MIPSGenerator.store()")
 
     def gen_instr(self, triple):
         """
@@ -153,7 +166,45 @@ class MIPSGenerator:
         example of this dispatch on a small triple program, including
         exactly which registers get allocated/reused/freed at each step.
         """
-        raise NotImplementedError("implement MIPSGenerator.gen_instr()")
+        if isinstance(triple, BinOpTriple):
+            src1 = triple.arg1
+            src2 = triple.arg2
+
+            if isinstance(src1, TripleRef):
+                src1 = self.triple_index_to_reg[src1.index]
+            else:
+                src1 = self.load(src1, self.allocate_registers())
+
+            if isinstance(src2, TripleRef):
+                src2 = self.triple_index_to_reg[src2.index]
+            else:
+                src2 = self.load(src2, self.allocate_registers())
+
+            dest = self.allocate_registers()
+            self.addMIPS(f"{MIPS_OP[triple.op]} {dest}, {src1}, {src2}")
+            self.triple_index_to_reg[triple.index] = dest
+            self.deallocate_register(src1)
+            self.deallocate_register(src2)
+
+        elif isinstance(triple, AssignTriple):
+            if isinstance(triple.arg1, TripleRef):
+                triple.arg1 = self.triple_index_to_reg[triple.arg1.index]
+            else:
+                triple.arg1 = self.load(triple.arg1, self.allocate_registers())
+
+            self.store(triple.arg1, triple.dest)
+
+        elif isinstance(triple, PrintTriple):
+            if isinstance(triple.arg1, TripleRef):
+                triple.arg1 = self.triple_index_to_reg[triple.arg1.index]
+            else:
+                triple.arg1 = self.load(triple.arg1, self.allocate_registers())
+
+            self.addMIPS(f"move $a0, {triple.arg1}")
+            self.addMIPS("li $v0, 1")
+            self.addMIPS("syscall")
+            self.deallocate_register(triple.arg1)
+        ##raise NotImplementedError("implement MIPSGenerator.gen_instr()")
 
     # ------------------------------------------------------------------
     # Prologue / epilogue -- PROVIDED, not a TODO. Exact sequence from
